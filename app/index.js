1b2cd93d71fe78debb7980dd85d84776b925fc86
Xavier Salazar <x.salazar1729@gmail.com>
2021-12-22 19:58:12 -0800

    First attempt at primitive image conversion to png images

--- app/index.js
+++ app/index.js
@@ -1,5 +1,17 @@
-const sharp = require('sharp')
+const sharp = require("sharp");
+const axios = require("axios");
 
-exports.handler = async(event, context) => {
-    
-}
+exports.handler = async (event, context) => {
+  if (event.imageSource) {
+    var response = await axios.get(event.imageSource, {
+      responseType: "arraybuffer",
+    });
+    var file = sharp(Buffer.from(response.data), {
+      density: 100000,
+    })
+      .resize(1024, 1024)
+      .png()
+      .toBuffer()
+    return Buffer.from(response.data, "binary").toString("base64");
+  }
+};
