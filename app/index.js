74283a8243484bde461940efa52b11d8bf5d866c
Xavier Salazar <x.salazar1729@gmail.com>
2021-12-22 20:11:08 -0800

    Try resizing the svg first

--- app/index.js
+++ app/index.js
@@ -6,12 +6,13 @@ exports.handler = async (event, context) => {
     var response = await axios.get(event.imageSource, {
       responseType: "arraybuffer",
     });
+
     var file = await sharp(Buffer.from(response.data), {
       density: 100000,
     })
-      .resize(1024, 1024)
       .png()
-      .toBuffer()
+      .resize(1024, 1024)
+      .toBuffer();
     return file.toString("base64");
   }
 };
