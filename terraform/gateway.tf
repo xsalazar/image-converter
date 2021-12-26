eb8e4ce722b874882d79315d15fbec245d0b2008
Xavier Salazar <x.salazar1729@gmail.com>
2021-12-25 19:30:22 -0800

    Remove duplicate resource

--- terraform/gateway.tf
+++ terraform/gateway.tf
@@ -31,11 +31,6 @@ resource "aws_apigatewayv2_stage" "instance" {
   api_id      = aws_apigatewayv2_api.instance.id
   name        = "$default"
   auto_deploy = true
-}
-
-resource "aws_apigatewayv2_stage" "instance" {
-  api_id = aws_apigatewayv2_api.instance.id
-  name   = "$default"
 
   default_route_settings {
     throttling_burst_limit = 50
