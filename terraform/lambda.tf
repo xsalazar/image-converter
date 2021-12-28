ebc341bef6b7f112fbc9f9adbb14dff8b73e8bdf
Xavier Salazar <x.salazar1729@gmail.com>
2021-12-28 12:09:09 -0800

    Add reference to CloudWatch Log Group to lower retention time

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -6,4 +6,18 @@ resource "aws_lambda_function" "instance" {
   runtime       = "nodejs14.x"
   timeout       = 30   // seconds
   memory_size   = 1536 // MB
+
+  // Since CI/CD will deploy this application externally, these do not need to be tracked after creation
+  lifecycle {
+    ignore_changes = [
+      last_modified,
+      source_code_hash,
+      source_code_size
+    ]
+  }
+}
+
+resource "aws_cloudwatch_log_group" "instance" {
+  name              = "/aws/lambda/${aws_lambda_function.instance.function_name}"
+  retention_in_days = 30 // days
 }
